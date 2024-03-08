75242939782e4731e76c4e7d12652ad4dbc704d3
Darmanbek <pahratdinovdarman@gmail.com>
2024-03-08 23:03:36 +0500

    Update 0.3

---
+++ src/components/SectionProfile/SectionProfile.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SectionProfile.scss"
+
+const SectionProfile: React.FC = () => {
+    return (
+        <section className="section-profile">
+            <div className="container">
+                <div className="profile-inner">
+                    
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default SectionProfile;
